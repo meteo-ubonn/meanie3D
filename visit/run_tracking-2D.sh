0dc195b187a794fd691ca8d072e03492afd08171
Jürgen Simon <simon@webtecc.com>
2014-01-05 16:52:12 +0100

    More work on freeing the run scripts from Visit

--- visit/run_tracking-2D.sh
+++ visit/run_tracking-2D.sh
@@ -39,9 +39,4 @@ ESCAPED_DL_PATH=$(echo $DL_PATH | sed -e "s/\//\\\\\//g")
 
 cat $MEANIE3D_HOME/visit/run_tracking-2D.py | sed -e "s/SOURCE_DIR/$ESCAPED_SOURCE_DIR/g" | sed -e "s/DL_PATH/$ESCAPED_DL_PATH/g" | sed -e "s/M3D_HOME/$ESCAPED_MEANIE3D_HOME/g" | sed -e "s/SCALE/$2/g" | sed -e "s/SKIP_VISUALIZATION/${SKIP_VISUALIZATION}/g" > $SCRIPTFILE
 
-if [ "${RUN_VISIT_HEADLESS}" = "true" ]
-then
-    ${VISIT_EXECUTABLE} -cli -nowin -s ${SCRIPTFILE}
-else
-    ${VISIT_EXECUTABLE} -s ${SCRIPTFILE}
-fi
+python ${SCRIPTFILE}
