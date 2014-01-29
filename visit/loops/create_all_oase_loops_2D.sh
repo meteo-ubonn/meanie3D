aef4f84c1ce9ac44dbed93bd139474cbe32db363
Jürgen Simon <simon@webtecc.com>
2014-01-29 21:35:44 +0100

    Adjusted Visit scripts to avoid overloads

--- visit/loops/create_all_oase_loops_2D.sh
+++ visit/loops/create_all_oase_loops_2D.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Check some pre-requisites
-
-if [ $# -lt 1 ]
+if [ "X$1" = "X" ]
 then
-    echo "create_all_oase_loops_2D <netcdf directory>"
-    echo "Creates loops of all oase composite variables and a composite loop"
+    echo "create_all_oase_loops_2D.sh <netcdf directory>"
+    echo "Visualize all variables in 2D OASE composite files"
     exit 0
 fi
 
-# scripts reachable?
+if [ "X${VISIT_EXECUTABLE}" = "X" ]
+then
+    echo "Please set environment variable VISIT_EXECUTABLE"
+    exit 0
+fi
 
 if [ "X${MEANIE3D_HOME}" = "X" ]
 then
@@ -17,22 +19,15 @@ then
     exit 0
 fi
 
-# netcdf directory exists?
+SCRIPTFILE="/tmp/cluster_visualization-$RANDOM.py"
+ESCAPED_NETCDF_DIR=$(echo $1 | sed -e "s/\//\\\\\//g")
+ESCAPED_MEANIE3D_HOME=$(echo $MEANIE3D_HOME | sed -e "s/\//\\\\\//g")
 
-if [ ! -d "$1" ]; then
-    echo "Can't find input directory $1"
-    exit 0
+cat $MEANIE3D_HOME/visit/loops/create_all_oase_loops_2D.py | sed -e "s/SOURCE_DIR_P/$ESCAPED_NETCDF_DIR/g" | sed -e "s/MEANIE3D_HOME_P/$ESCAPED_MEANIE3D_HOME/g" > ${SCRIPTFILE}
+
+if [ "${RUN_VISIT_HEADLESS}" = "true" ]
+then
+    ${VISIT_EXECUTABLE} -cli -nowin -s ${SCRIPTFILE}
+else
+    ${VISIT_EXECUTABLE} -s ${SCRIPTFILE}
 fi
-
-# Visualize variables individually
-
-variables=(cband_radolan_rx linet_oase_tl msevi_l15_ir_108 msevi_l15_vis006 msevi_l2_cmsaf_cot msevi_l2_cmsaf_cph msevi_l2_cmsaf_cwp msevi_l2_cmsaf_reff msevi_l2_nwcsaf_cma msevi_l2_nwcsaf_crr msevi_l2_nwcsaf_ct msevi_l2_nwcsaf_cth)
-
-for i in "${variables[@]}"
-do
-    ${MEANIE3D_HOME}/visit/loops/create_oase_loop_2D.sh $1 $i
-done
-
-# Run the composite 
-
-${MEANIE3D_HOME}/visit/loops/create_composite_loop_2D.sh $1
