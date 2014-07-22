304a3671dfe98cfa5828db53468c54174a923d94
Jürgen Simon <simon@webtecc.com>
2014-07-22 10:37:38 +0200

    Added a script for running stats in one directory only

---
+++ visit/statistics/create_stats.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "X${MEANIE3D_HOME}" = "X" ]
+then
+    echo "Please set environment variable MEANIE3D_HOME"
+    exit 0
+fi
+
+meanie3D-trackstats --write-track-dictionary --write-gnuplot-files \
+            --create-length-statistics --create-speed-statistics \
+            --create-direction-statistics --create-cluster-statistics \
+            --write-center-tracks-as-vtk --vtk-dimensions=x y \
+            -b  -p netcdf
+
+${MEANIE3D_HOME}/visit/statistics/plot-stats.sh
+${MEANIE3D_HOME}/visit/visualize_results/visualize_radolan_tracks.sh .
