31623584e01fac2089040e8b61f4854a21b77029
Juergen Simon <juergen.simon@uni-bonn.de>
2014-11-28 15:49:26 +0100

    Added license information

--- include/meanie3D/numericalrecipes/nrutil.h
+++ include/meanie3D/numericalrecipes/nrutil.h
@@ -1,10 +1,3 @@
-/* CAUTION: This is the traditional K&R C (only) version of the Numerical
-   Recipes utility file nrutil.h.  Do not confuse this file with the
-   same-named file nrutil.h that is supplied in the 'misc' subdirectory.
-   *That* file is the one from the book, and contains both ANSI and
-   traditional K&R versions, along with #ifdef macros to select the
-   correct version.  *This* file contains only traditional K&R.           */
-
 #ifndef _NR_UTILS_H_
 #define _NR_UTILS_H_
 
