84c750ed5ff76ba981ac1228bcd3a0baaa1f02be
Jürgen Simon <simon@webtecc.com>
2015-10-21 15:22:35 +0100

    issueid #376 Cleaning up.

---
+++ python/meanie3D/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'Juergen Simon'
+__email__ = 'juergen.simon@uni-bonn.de'
+__version__ = '1.5.4'
+__url__ = 'http://git.meteo.uni-bonn.de/projects/meanie3d'
+__all__ = ['app', 'visualisation']
