6508a6c4c43ca7e868fcde425123eba48120fd6b
Jürgen Simon <simon@webtecc.com>
2015-11-04 16:50:44 +0000

    issueid #388 Polished off the track graph with navigation.

--- python/meanie3D/resources/meanie3d.js
+++ python/meanie3D/resources/meanie3d.js
@@ -2,6 +2,10 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
 
     M3D = {};
 
+    M3D.graphPercentWidth = 0.85;
+    M3D.radius = 12;
+    M3D.color = d3.scale.category20();
+
     /**
      * Class representing a graph of tracks constructed
      * from the data in a tracking dictionary. Also contains
@@ -11,7 +15,7 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
 	function TrackingGraph(dictionary, filterById, filterByTrackLength) {
 	    // Data
 	    this.dictionary = dictionary;
-	    this.id = false;
+	    this.selectedId = false;
 		this.nodes = dictionary.tree.nodes;
 		this.links = dictionary.tree.links;
 		// Filtering and indexing
@@ -20,8 +24,8 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
 		this.ids = new Array();
 
 		this.trackLengthIndex = {
-		    values : new Array(),
-		    map : new Map()
+		    values : new Array(),   // distinct track lengths
+		    map : new Map()         // ids of the tracks in each class
 		}
 		this.selectedTrackLength = false;
 
@@ -30,7 +34,7 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
          * @param {number} id
          */
         this.setId = function(filterById) {
-            this.id = filterById;
+            this.selectedId = filterById;
             if (filterById) {
                 this.linkedNodes = this.getLinkedNodesByID(parseInt(filterById));
             }
@@ -62,7 +66,7 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
 
         /** @return a function to filter nodes. */
         this.nodeFilter = function(node) {
-            if (this.id || this.selectedTrackLength) {
+            if (this.selectedId || this.selectedTrackLength) {
 		        return this.linkedNodes.has(node);
             }
             return true;
@@ -70,7 +74,7 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
 
         /** @return a function to filter links. */
 	    this.linkFilter = function(link) {
-	        if (this.id || this.selectedTrackLength) {
+	        if (this.selectedId || this.selectedTrackLength) {
                 return this.linkedNodes.has(link.source) || this.linkedNodes.has(link.target);
             }
             return true;
@@ -134,6 +138,43 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
                     nodeSet.add(node);
             });
             return nodeSet;
+        }
+
+        /**
+         * Figures out what the id of the node with the given uuid is.
+         * @param uuid
+         * @returns id
+         */
+        this.findIdForUuid = function(uuid) {
+            var id;
+            for (var i=0; i < this.nodes.length && !id; i++) {
+                var node = this.nodes[i];
+                if (node.uuid == uuid) {
+                    id = node.id;
+                }
+            }
+            return id;
+        }
+
+        /**
+         * Finds a cluster with the given uuid.
+         * @param uuid
+         * @returns cluster or false.
+         */
+        this.getClusterForNode = function(node) {
+            var cluster = false;
+            for (var ti = 0; ti < this.dictionary.tracks.length && !cluster; ti++) {
+                var t = this.dictionary.tracks[ti];
+                if (t.id == node.id) {
+                    for (var ci = 0; ci < t.clusters.length && !cluster; ci++) {
+                        var c = t.clusters[ci];
+                        if (c.uuid == node.uuid) {
+                            cluster = c;
+                        }
+                    }
+                }
+            };
+            return cluster;
         }
 
         /**
@@ -201,7 +242,8 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
             });
             // index track dictionary
             this.dictionary.tracks.forEach(function(track) {
-                if (self.trackLengthIndex.values.indexOf(track.length) < 0) {
+                var tli = self.trackLengthIndex.values.indexOf(track.length);
+                if ( tli < 0) {
                     self.trackLengthIndex.values.push(track.length);
                 }
                 var trackIds = self.trackLengthIndex.map.get(track.length);
@@ -215,16 +257,320 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
             this.trackLengthIndex.values.sort(function(a,b) {
                 return (a > b);
             });
-            console.log(this.trackLengthIndex.values);
         };
 
         // Finally index the data
         this.resolveLinks();
 		this.index();
 		this.setId(filterById);
-		this.setTrackLength(filterByTrackLength)
+		if (!filterByTrackLength) {
+		    // unless an id was selected, default to the first
+		    // entry in the track lengths
+		    if (!this.selectedId) {
+                this.setTrackLength(this.trackLengthIndex.values[0]);
+            }
+		} else {
+            this.setTrackLength(filterByTrackLength)
+        }
     }
     M3D.TrackingGraph = TrackingGraph;
+
+
+    /**
+     * Adds a div#menu to the page, which contains the navigational
+     * bubble menu.
+     */
+    M3D.addBubbleMenu = function(graph) {
+        // Extract a linear index as objects
+        var children = new Array();
+        for (var i = 0; i < graph.trackLengthIndex.values.length; i++) {
+            var len = graph.trackLengthIndex.values[i];
+            children.push({
+                "length": len,
+                "count": graph.trackLengthIndex.map.get(len).size
+            });
+        }
+        var dataset = {
+            "children": children
+        }
+
+        var size = M3D.getMenuSize();
+        var diameter = Math.min(size.width, size.height);
+
+        var svg = d3.select("body")
+            .select("#menu")
+            .append("svg")
+            .attr("width", size.width)
+            .attr("height", size.height)
+            .attr("class", "bubble");
+
+        var format = d3.format(",d");
+        var color = d3.scale.category20c();
+
+        var bubble = d3.layout.pack()
+            .sort(null)
+            .size([diameter, diameter])
+            .padding(1.5)
+            .value(function(d) { return d.length; })
+
+        var node = svg.selectAll(".node")
+            .data(bubble.nodes(dataset))
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+        node.append("title")
+            .text(function(d) {
+                return d.length;
+             });
+
+        node.append("circle")
+            .attr("r", function(d) { return 10 * Math.log(1+d.length); })
+            .style("fill", function(d) { return color(d.length); });
+
+        node.append("text")
+            .attr("dy", ".3em")
+            .style("text-anchor", "middle")
+            .text(function(d) {
+                return d.count;
+             });
+
+         d3.select(self.frameElement).style("height", diameter + "px");
+    }
+
+    /**
+     * Adds a div#menu to the page, which contains the navigational
+     * bar chart
+     */
+    M3D.addBarchartMenu = function(graph) {
+
+        // Extract a linear index as objects
+        var classes = new Array();
+        var maxCount = -1;
+        var maxLength = -1;
+        for (var i = 0; i < graph.trackLengthIndex.values.length; i++) {
+            var len = graph.trackLengthIndex.values[i];
+            var count = graph.trackLengthIndex.map.get(len).size;
+            classes.push({
+                length: len,
+                count: count
+            });
+            if (count > maxCount) maxCount = count;
+            if (len > maxLength) maxLength = len;
+        }
+        classes.sort(function(a,b) {
+            return (a.length > b.length);
+        });
+        var color = d3.scale.ordinal(maxCount);
+
+        var size = M3D.getMenuSize();
+        var menu = d3.select("body")
+            .select("#menu")
+            .select("#barchart")
+            .append("div")
+
+        menu.selectAll("div")
+            .data(classes)
+            .enter()
+            .append("div")
+            .attr("class", function(d) {
+                return d.length == graph.selectedTrackLength ? "bar selected" : "bar";
+            })
+            .style("width", function(d) {
+                var width = size.width * (maxCount - d.count) / maxCount - 20;
+                return width + "px";
+            })
+            .style("opacity", function(d) {
+                return color(d.count);
+            })
+            .on("click", function(d) {
+                M3D.filterByTrackLength(d.length);
+            })
+            .text(function(d) {
+                return d.length + " (" + d.count + ")";
+            })
+
+        if (graph.selectedId) {
+            d3.select("#menu")
+                .select("#top")
+                .append("div")
+                .attr("id","trackinfo")
+                .text("Selected track #" + graph.selectedId);
+        }
+
+    }
+
+    /**
+     * Adds a div#graph to the page, which contains the actual graph.
+     */
+    M3D.addTrackingGraph = function(graph) {
+        var size = M3D.getGraphSize();
+
+        var force = d3.layout.force()
+            .charge(-200)
+            .linkDistance(50)
+            .size([size.width, size.height]);
+
+        var svg = d3.select("body")
+            .select("#graph")
+            .append("svg");
+
+        var nodes = graph.filteredNodes();
+        var links = graph.filteredLinks();
+
+        force
+            .nodes(nodes)
+            .links(links)
+            .start();
+
+        var link = svg.selectAll(".link")
+            .data(links)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke", function(d) {
+                if (d.type == 0) {
+                    return "lightgrey"
+                } else if (d.type == 1) {
+                    return "red";
+                } else {
+                    return "blue";
+                }
+            })
+            .style("stroke-width", function(d) {
+                return 2;
+            });
+
+        var node = svg.selectAll(".node")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("r", function(d) {
+                return 2.5 * Math.log(1.0 + d.size);
+            })
+            .style("fill", function(d) { return M3D.color(d.id); })
+            .on("dblclick", function(d) {
+                M3D.filterById(d.id);
+            })
+            .on("mouseover", function(d) {
+                var coordinates = d3.mouse(this);
+                M3D.showNodeOverlay(graph,d,coordinates);
+            })
+            .on("mouseout", function(d) {
+                M3D.hideNodeOverlay();
+            })
+            .call(force.drag);
+
+        var label = svg.selectAll("text")
+            .data(nodes)
+            .enter().append("text")
+            .text(function(d) {
+                return d.id;
+                // return d.uuid + "/" + d.id;
+            })
+            .attr("fill","black")
+            .attr("text-anchor", "middle")
+            .attr("text-align", "center")
+            .attr("alignment-baseline","central")
+            .attr("font-weight", "bold");
+
+        node.append("title")
+            .text(function(d) {
+                return d.name;
+            });
+
+        force.on("tick", function() {
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; })
+                .attr("opacity", function(d) {
+                    return 0.25 + 0.75 * (d.source.step / graph.steps.length);
+                });
+
+
+            node
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; })
+                .attr("opacity", function(d) {
+                    return 0.25 + 0.75 * (d.step / graph.steps.length);
+                });
+
+            label
+                .attr("x", function(d, i) {
+                    return d.x;
+                })
+                .attr("y", function(d, i) {
+                    return d.y;
+                });
+        });
+    }
+
+    M3D.showNodeOverlay = function(graph, node, coordinates) {
+        var overlay = d3.select("#node-overlay");
+        overlay.attr("display","none");
+        var cluster = graph.getClusterForNode(node);
+        if (cluster) {
+            overlay.select("#node-data").remove();
+            var list = overlay
+                .append("ul")
+                .attr("id","node-data");
+            list.append("li").text("id: " + node.id);
+            list.append("li").text("uuid: " + cluster.uuid);
+            list.append("li").text("size: " + cluster.size);
+            list.append("li").text("mins: " + cluster.min);
+            list.append("li").text("maxs: " + cluster.max);
+            list.append("li").text("medians: " + cluster.median);
+            list.append("li").text("margin points: " + cluster.has_margin_points);
+            list.append("li").text("source: " + cluster.sourcefile);
+            list.append("li").text("center: " + cluster.geometrical_center);
+        }
+
+        var x = coordinates[0] + 200;
+        var y = coordinates[1] - 200;
+
+        overlay
+            .style("background-color", M3D.color(node.id))
+            .style("display","block")
+            .style("opacity", 0.8)
+            .style("position","absolute")
+            .style("left",x + "px")
+            .style("top", y + "px");
+    }
+
+    M3D.hideNodeOverlay = function() {
+        var overlay = d3.select("#node-overlay");
+        overlay.style("display","none");
+    }
+
+
+    M3D.getViewportSize = function() {
+        var size = {
+            'width' : Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+            'height' : Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+        }
+        return size;
+    }
+
+    M3D.getGraphSize = function() {
+        var viewport = M3D.getViewportSize();
+        var size = {
+            'width' : M3D.graphPercentWidth * viewport.width,
+            'height' : viewport.height
+        }
+        return size;
+    }
+
+    M3D.getMenuSize = function() {
+        var viewport = M3D.getViewportSize();
+        var size = {
+            'width' : (1.0 - M3D.graphPercentWidth) * viewport.width,
+            'height' : viewport.height
+        }
+        return size;
+    }
 
     /**
      * Courtesy of https://css-tricks.com/snippets/javascript/get-url-variables/
@@ -240,6 +586,20 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
        return(false);
     }
 
+    M3D.resize = function() {
+        var menuSize = M3D.getMenuSize();
+        d3.select("#menu")
+            .style("width", menuSize.width)
+            .style("height", menuSize.height);
+
+        var graphSize = M3D.getGraphSize();
+
+        d3.select("#graph").select("svg")
+            .attr("width", graphSize.width)
+            .attr("height", graphSize.height);
+
+    }
+
     /**
      * Reloads the current page with an id for filtering.
      * @param A node or link or any other object with property 'id'.
@@ -250,7 +610,7 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
         if ( i > 0) {
             location = location.substring(0,i);
         }
-        window.location = location + "?id=" + d;
+        window.location.href = location + "?id="+d;
     }
 
     M3D.filterByTrackLength = function(d) {
@@ -259,6 +619,17 @@ if (typeof (M3D) == 'undefined' || M3D == null) {
         if ( i > 0) {
             location = location.substring(0,i);
         }
-        window.location = location + "?tracklength=" + d;
+        window.location.href = (location + "?tracklength=" + d);
     }
+
+    M3D.search = function(graph,id,uuid) {
+        if (id) {
+            M3D.filterById(id);
+        } else if (uuid) {
+            var _id = graph.findIdForUuid(uuid);
+            M3D.filterById(_id);
+        }
+        return false;
+    }
+
 }
