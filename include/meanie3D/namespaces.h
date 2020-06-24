fdbc276a1ec16925d6f19764eb1ef4e010597082
Jürgen Simon <simon@webtecc.com>
2020-06-24 13:24:32 +0100

    Fixed import problem

--- include/meanie3D/namespaces.h
+++ include/meanie3D/namespaces.h
@@ -24,10 +24,6 @@
 #ifndef M3D_NAMESPACE_DEFINITIONS_H
 #define M3D_NAMESPACE_DEFINITIONS_H
 
-#include <iostream>             // namespace std
-#include <netcdf>               // namespace netCDF
-#include <boost/assert.hpp>     // namespace boost;
-
 // This file manages the imports of namespaces into the 
 // various namespaces defined in this software.
 // That way all dependencies can be changed in one place
